5ed2001bb9ac1cb2044b4fc01f41678a1adb7390
Aaron Feledy <aaron@arrowonesolutions.com>
2017-10-06 15:39:16 -0500

    Add SSH script

---
+++ ssh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## SSH connection handler file
+## This file is used to establish remote SSH connections.
+## ------------------------------------------------------------
+
+# This script connects to remote servers.
+REMOTE_TASKS=true
+
+# Bootstrap
+SCRIPTPATH="${0%/*}"; source $SCRIPTPATH/bootstrap.sh
+
+echo "Connecting to $SSH_USER@$SSH_SERVER..."
+
+# Start SSH connection
+ssh -t $SSH_USER@$SSH_SERVER "cd $SSH_REM_DIR; exec \$SHELL -l"
