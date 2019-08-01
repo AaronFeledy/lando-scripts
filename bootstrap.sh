2924e58a4776c58a29a5a6374427027eba3b8083
Aaron Feledy <aaron@arrowonesolutions.com>
2019-08-01 13:43:18 -0500

    Do not allow direct execution of bootstrap and options scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 # Bootstrap file
 # Bootstraps various scripts by loading options and other functions.
 # ------------------------------------------------------------
+
+# This script is not meant to be run directly. It should be called by a lando-scripts script.
+[ $(basename "$0") != "bootstrap.sh" ] || echo "[ERROR] This script cannot be executed directly." && exit
   
 # Init variables
 SCRIPTPATH="${0%/*}"  # Path of this script
