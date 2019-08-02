89433f696b264ebc11cec9989878b4469386992f
Aaron Feledy <aaron@arrowonesolutions.com>
2019-08-02 16:25:43 -0500

    Allow prompting for username in pull-database script

--- pull-database.sh
+++ pull-database.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# This script connects to remote servers.
-REMOTE_TASKS=true
-
 read -p "Overwrite your local database? (y/n) " -n 1 -r
 echo
 
@@ -13,15 +10,21 @@ then
   # Load common functions and variables
   SCRIPTPATH=$(dirname "${BASH_SOURCE[0]}") && source $SCRIPTPATH/common/init.sh
 
+  include remote
+
+  # We need a remote username to continue
+  REMOTE_USER="$(remote_user)"
+  [ ! -z "$REMOTE_USER" ] || die "A username is required!"
+
   if $DB_REM_SCRIPT ; then
     # Execute tasks on remote server
-    ssh -tt -p $SSH_PORT $SSH_USER@$SSH_SERVER "/bin/bash -sl -- \"$SSH_REM_DIR\" \"$DB_REM_BACKUP_CMD\"" < $SCRIPTPATH/pull-database-remote.sh
+    ssh -tt -p $SSH_PORT $REMOTE_USER@$SSH_SERVER "/bin/bash -sl -- \"$SSH_REM_DIR\" \"$DB_REM_BACKUP_CMD\"" < $SCRIPTPATH/pull-database-remote.sh
     
     # Download db dump
-    scp -P $SSH_PORT $SSH_USER@$SSH_SERVER:$DB_REM_DUMP_FILE.gz .
+    scp -P $SSH_PORT $REMOTE_USER@$SSH_SERVER:$DB_REM_DUMP_FILE.gz .
     
     # Delete db dump on remote
-    ssh -t -p $SSH_PORT $SSH_USER@$SSH_SERVER "rm -fv $DB_REM_DUMP_FILE.gz"
+    ssh -t -p $SSH_PORT $REMOTE_USER@$SSH_SERVER "rm -fv $DB_REM_DUMP_FILE.gz"
   fi
 	
   # Import db dump
