101172a23309ec889e421926cf407c1acb1283ab
Aaron Feledy <aaron@arrowonesolutions.com>
2019-08-06 23:40:30 -0500

    pull-db script should respect option to not execute remote db dump

--- pull-database.sh
+++ pull-database.sh
@@ -16,15 +16,24 @@ then
   REMOTE_USER="$(remote_user)"
   [ ! -z "$REMOTE_USER" ] || die "A username is required!"
 
+  function download_db() {
+    # Download db dump from remote server
+    scp -P $SSH_PORT $REMOTE_USER@$SSH_SERVER:$DB_REM_DUMP_FILE .
+  }
+
+
   if $DB_REM_SCRIPT ; then
-    # Execute tasks on remote server
+    # Execute tasks on remote server to dump db.
     ssh -tt -p $SSH_PORT $REMOTE_USER@$SSH_SERVER "/bin/bash -sl -- \"$SSH_REM_DIR\" \"$DB_REM_BACKUP_CMD\"" < $SCRIPTPATH/pull-database-remote.sh
     
-    # Download db dump
-    scp -P $SSH_PORT $REMOTE_USER@$SSH_SERVER:$DB_REM_DUMP_FILE.gz .
+    download_db
     
-    # Delete db dump on remote
-    ssh -t -p $SSH_PORT $REMOTE_USER@$SSH_SERVER "rm -fv $DB_REM_DUMP_FILE.gz"
+    # Delete newly created db dump on remote.
+    ssh -t -p $SSH_PORT $REMOTE_USER@$SSH_SERVER "rm -fv $DB_REM_DUMP_FILE"
+
+  else
+    # Download db dump that already exists.
+    download_db
   fi
 	
   # Import db dump
